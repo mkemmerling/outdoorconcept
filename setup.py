5d3e9e2cf6c88540b4fbc93171c03985058be565
Markus Kemmerling <markus.kemmerling@meduniwien.ac.at>
2015-01-12 13:15:22 +0100

    Require Django.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='outdoorconcept',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='outdoorconcept Application',
+      author='Markus Kemmerling',
+      author_email='kemmerling@gmx.at',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Django==1.7.2'],
      )
