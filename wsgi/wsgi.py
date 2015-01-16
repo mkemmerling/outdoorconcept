7fa20dea24bb794ee7b0837f25353aba89874442
Markus Kemmerling <markus.kemmerling@meduniwien.ac.at>
2015-01-16 14:38:36 +0100

    Test

--- wsgi/wsgi.py
+++ wsgi/wsgi.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python
 import os
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
-
-def application(environ, start_response):
-
-    ctype = 'text/plain'
-    response_body = ['%s: %s' % (key, value)
-                for key, value in sorted(environ.items())]
-    response_body = '\n'.join(response_body)
-    status = '200 OK'
-    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-    #
-    start_response(status, response_headers)
-    return [response_body]
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
