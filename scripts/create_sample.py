f687dd208cce0b5f6361b813d4542b053dd1b0a5
Markus Kemmerling <markus.kemmerling@meduniwien.ac.at>
2015-01-16 08:07:15 +0100

    Working on database creation scripts.

--- scripts/create_sample.py
+++ scripts/create_sample.py
@@ -4,7 +4,7 @@ import sys
 
 wd = os.path.join(os.path.dirname(sys.argv[0]), os.path.pardir)
 sys.path.insert(0, os.path.abspath(wd))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 import django
 django.setup()
@@ -12,4 +12,4 @@ django.setup()
 from ropeelements.factories import create_kinds, create_elements
 
 create_kinds()
-create_elements()
+# create_elements()
