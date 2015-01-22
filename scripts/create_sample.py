e3e72faa96a6ef8118419a7cf2d6d13dbd9ad5b0
Markus Kemmerling <markus.kemmerling@meduniwien.ac.at>
2015-01-22 07:06:01 +0100

    Recreate appcache manifest on sample creation.

--- scripts/create_sample.py
+++ scripts/create_sample.py
@@ -9,7 +9,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 import django
 django.setup()
 
+from main.management.appcache import create_manifest
 from ropeelements.factories import create_kinds, create_elements
 
 create_kinds()
 create_elements()
+create_manifest()
