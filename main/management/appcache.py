cf9ca8f3191874135ebf9124d0b6e236ef09a6b7
Markus Kemmerling <markus.kemmerling@meduniwien.ac.at>
2015-01-25 18:41:28 +0100

    Test commit

--- main/management/appcache.py
+++ main/management/appcache.py
@@ -24,22 +24,16 @@ def create_manifest():
     manifest = 'CACHE MANIFEST\n'
     manifest += created() + '\n\n'
 
-    manifest += '# Data\n'
-    manifest += '/en/api/ropeelements\n'
-    manifest += '/de/api/ropeelements\n'
-
-    manifest += '\n'
-    manifest += 'NETWORK:\n'
-    manifest += '*\n'
-
-    manifest += '\n'
-    manifest += 'CACHE:\n'
-
     manifest += '# Templates\n'
     manifest += '/en/ropeelements\n'
     manifest += '/de/seilelemente\n'
     manifest += '/en/ng/ropeelements\n'
     manifest += '/de/ng/ropeelements\n'
+    manifest += '\n'
+
+    manifest += '# Data\n'
+    manifest += '/en/api/ropeelements\n'
+    manifest += '/de/api/ropeelements\n'
     manifest += '\n'
 
     manifest += '# Static files\n'
@@ -101,6 +95,10 @@ def create_manifest():
         names = '\n'.join(join(base, name) for name in files)
         if names:
             manifest += names + '\n'
+
+    manifest += '\n'
+    manifest += 'NETWORK:\n'
+    manifest += '*\n'
 
     with open(MANIFEST_FILE, 'w') as fd:
         fd.write(manifest)
--- ropeelements/static/ropeelements/js/controllers.js
+++ ropeelements/static/ropeelements/js/controllers.js
@@ -31,75 +31,77 @@ angular.module('outdoorconcept.ropeelement.controllers', ['ngResource'])
 
         $scope.kinds = [];
 
-        RopeElement.query({language: language.getLanguage()}).$promise.then(function (result) {
-            $scope.ropeelements = result;
-            angular.forEach(result, function (kind_elements) {
-                $scope.kinds.push(kind_elements.kind);
-                kinds_by_id[kind_elements.kind.id] = kind_elements.kind;
-                elements_by_kind[kind_elements.kind.id] = kind_elements.elements;
-            });
+        $scope.ropeelements = [];
 
-            $scope.filter = angular.fromJson(storage.getItem('elementFilter'));
-            if (!$scope.filter) {
-                $scope.filter = empty_filter;
-                storage.setItem('elementFilter', angular.toJson($scope.filter));
-            }
+        // RopeElement.query({language: language.getLanguage()}).$promise.then(function (result) {
+        //     $scope.ropeelements = result;
+        //     angular.forEach(result, function (kind_elements) {
+        //         $scope.kinds.push(kind_elements.kind);
+        //         kinds_by_id[kind_elements.kind.id] = kind_elements.kind;
+        //         elements_by_kind[kind_elements.kind.id] = kind_elements.elements;
+        //     });
 
-            if (!angular.equals($scope.filter, empty_filter)) {
-                queryRopeElements();
-            }
+        //     $scope.filter = angular.fromJson(storage.getItem('elementFilter'));
+        //     if (!$scope.filter) {
+        //         $scope.filter = empty_filter;
+        //         storage.setItem('elementFilter', angular.toJson($scope.filter));
+        //     }
 
-            // Prevent filtering and display of no results message until loaded
-            $timeout(function () {
-                $scope.loaded = true;
-            });
-        });
+        //     if (!angular.equals($scope.filter, empty_filter)) {
+        //         queryRopeElements();
+        //     }
 
-        function queryRopeElements() {
-            var set_boolean_filters, kinds;
+        //     // Prevent filtering and display of no results message until loaded
+        //     $timeout(function () {
+        //         $scope.loaded = true;
+        //     });
+        // });
 
-            set_boolean_filters = $.grep(boolean_filters, function (name) {
-                return $scope.filter[name];
-            });
+        // function queryRopeElements() {
+        //     var set_boolean_filters, kinds;
 
-            function getElements(kind) {
-                return $.grep(elements_by_kind[kind.id], function (element) {
-                    return ($.grep(set_boolean_filters, function (name) {
-                        return element[name];
-                    }).length === set_boolean_filters.length) && element;
-                });
-            }
+        //     set_boolean_filters = $.grep(boolean_filters, function (name) {
+        //         return $scope.filter[name];
+        //     });
 
-            // We track kinds by their ids to ensure that the selected kind is
-            // preserved on language switch. Also note that in rare cases
-            // a kind might be gone after reload.
-            if ($scope.filter.kind && kinds_by_id[$scope.filter.kind.id] !== undefined) {
-                kinds = [kinds_by_id[$scope.filter.kind.id]];
-            } else {
-                kinds = $scope.kinds;
-            }
+        //     function getElements(kind) {
+        //         return $.grep(elements_by_kind[kind.id], function (element) {
+        //             return ($.grep(set_boolean_filters, function (name) {
+        //                 return element[name];
+        //             }).length === set_boolean_filters.length) && element;
+        //         });
+        //     }
 
-            $scope.ropeelements = [];
-            angular.forEach(kinds, function (kind) {
-                var elements = getElements(kind);
-                if (elements.length > 0) {
-                    $scope.ropeelements.push({
-                        kind: kind,
-                        elements: elements
-                    });
-                }
-            });
-        }
+        //     // We track kinds by their ids to ensure that the selected kind is
+        //     // preserved on language switch. Also note that in rare cases
+        //     // a kind might be gone after reload.
+        //     if ($scope.filter.kind && kinds_by_id[$scope.filter.kind.id] !== undefined) {
+        //         kinds = [kinds_by_id[$scope.filter.kind.id]];
+        //     } else {
+        //         kinds = $scope.kinds;
+        //     }
 
-        angular.forEach(filters, function (name) {
-            $scope.$watch('filter.' + name, function () {
-                // Prevent query on first watch
-                if ($scope.loaded) {
-                    queryRopeElements();
-                    storage.setItem('elementFilter', angular.toJson($scope.filter));
-                }
-            });
-        });
+        //     $scope.ropeelements = [];
+        //     angular.forEach(kinds, function (kind) {
+        //         var elements = getElements(kind);
+        //         if (elements.length > 0) {
+        //             $scope.ropeelements.push({
+        //                 kind: kind,
+        //                 elements: elements
+        //             });
+        //         }
+        //     });
+        // }
+
+        // angular.forEach(filters, function (name) {
+        //     $scope.$watch('filter.' + name, function () {
+        //         // Prevent query on first watch
+        //         if ($scope.loaded) {
+        //             queryRopeElements();
+        //             storage.setItem('elementFilter', angular.toJson($scope.filter));
+        //         }
+        //     });
+        // });
     }
 ]);
 
