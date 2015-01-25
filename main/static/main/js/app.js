ecc174fe39ec718ad295efb41d74ed8662c214ef
Markus Kemmerling <markus.kemmerling@meduniwien.ac.at>
2015-01-25 09:24:13 +0100

    Test commit

--- main/static/main/js/app.js
+++ main/static/main/js/app.js
@@ -48,8 +48,6 @@ angular.module('outdoorconcept.base', [])
 
         $scope.debug_msg = "–––";
 
-        console.log("TEST!!!");
-
         cache.addEventListener('noupdate', function () {
             console.warn("No manifest update");
             $scope.debug_msg = "No manifest update";
