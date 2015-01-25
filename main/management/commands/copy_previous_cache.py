b193eb3d03537048061849fe388b4aff16355622
Markus Kemmerling <markus.kemmerling@meduniwien.ac.at>
2015-01-25 11:21:26 +0100

    Still trying to get copy of previous static cache right.

--- main/management/commands/copy_previous_cache.py
+++ main/management/commands/copy_previous_cache.py
@@ -1,18 +1,21 @@
 import os
 import shutil
+import tempfile
 
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
 
 
 class Command(NoArgsCommand):
-    help = 'Copy previously cached static files to current static cache.'
+    help = ('Copy previously cached static files to current static cache and '
+            'save current one.')
 
     def handle_noargs(self, **options):
         previous_cache = os.path.join(settings.DATA_DIR, 'PREVIOUS_CACHE')
         cache_dir = os.path.join(settings.STATIC_ROOT, 'CACHE')
+        tempdir = tempfile.TemporaryDirectory(prefix='oc_')
 
-        def copy_files(fype):
+        def copy_previous(fype):
             src_dir = os.path.join(previous_cache, fype)
             dest_dir = os.path.join(cache_dir, fype)
             if os.path.exists(src_dir):
@@ -23,5 +26,10 @@ class Command(NoArgsCommand):
                         if not os.path.exists(dst):
                             shutil.copy(src, dst)
 
-        copy_files('css')
-        copy_files('js')
+        shutil.copytree(cache_dir, tempdir.name)
+        if os.path.exists(previous_cache):
+            copy_previous('css')
+            copy_previous('js')
+            shutil.rmtree(previous_cache)
+            new_cache = os.path.join(tempdir.name, 'CACHE')
+            shutil.copytree(new_cache, previous_cache)
