dc78b1032e1bf01c2c0eec5f5deea2a8b39c1141
Markus Kemmerling <markus.kemmerling@meduniwien.ac.at>
2015-01-26 05:21:24 +0100

    Reverted test commit

--- ropeelements/static/ropeelements/js/controllers.js
+++ ropeelements/static/ropeelements/js/controllers.js
@@ -31,77 +31,75 @@ angular.module('outdoorconcept.ropeelement.controllers', ['ngResource'])
 
         $scope.kinds = [];
 
-        $scope.ropeelements = [];
+        RopeElement.query({language: language.getLanguage()}).$promise.then(function (result) {
+            $scope.ropeelements = result;
+            angular.forEach(result, function (kind_elements) {
+                $scope.kinds.push(kind_elements.kind);
+                kinds_by_id[kind_elements.kind.id] = kind_elements.kind;
+                elements_by_kind[kind_elements.kind.id] = kind_elements.elements;
+            });
 
-        // RopeElement.query({language: language.getLanguage()}).$promise.then(function (result) {
-        //     $scope.ropeelements = result;
-        //     angular.forEach(result, function (kind_elements) {
-        //         $scope.kinds.push(kind_elements.kind);
-        //         kinds_by_id[kind_elements.kind.id] = kind_elements.kind;
-        //         elements_by_kind[kind_elements.kind.id] = kind_elements.elements;
-        //     });
+            $scope.filter = angular.fromJson(storage.getItem('elementFilter'));
+            if (!$scope.filter) {
+                $scope.filter = empty_filter;
+                storage.setItem('elementFilter', angular.toJson($scope.filter));
+            }
 
-        //     $scope.filter = angular.fromJson(storage.getItem('elementFilter'));
-        //     if (!$scope.filter) {
-        //         $scope.filter = empty_filter;
-        //         storage.setItem('elementFilter', angular.toJson($scope.filter));
-        //     }
+            if (!angular.equals($scope.filter, empty_filter)) {
+                queryRopeElements();
+            }
 
-        //     if (!angular.equals($scope.filter, empty_filter)) {
-        //         queryRopeElements();
-        //     }
+            // Prevent filtering and display of no results message until loaded
+            $timeout(function () {
+                $scope.loaded = true;
+            });
+        });
 
-        //     // Prevent filtering and display of no results message until loaded
-        //     $timeout(function () {
-        //         $scope.loaded = true;
-        //     });
-        // });
+        function queryRopeElements() {
+            var set_boolean_filters, kinds;
 
-        // function queryRopeElements() {
-        //     var set_boolean_filters, kinds;
+            set_boolean_filters = $.grep(boolean_filters, function (name) {
+                return $scope.filter[name];
+            });
 
-        //     set_boolean_filters = $.grep(boolean_filters, function (name) {
-        //         return $scope.filter[name];
-        //     });
+            function getElements(kind) {
+                return $.grep(elements_by_kind[kind.id], function (element) {
+                    return ($.grep(set_boolean_filters, function (name) {
+                        return element[name];
+                    }).length === set_boolean_filters.length) && element;
+                });
+            }
 
-        //     function getElements(kind) {
-        //         return $.grep(elements_by_kind[kind.id], function (element) {
-        //             return ($.grep(set_boolean_filters, function (name) {
-        //                 return element[name];
-        //             }).length === set_boolean_filters.length) && element;
-        //         });
-        //     }
+            // We track kinds by their ids to ensure that the selected kind is
+            // preserved on language switch. Also note that in rare cases
+            // a kind might be gone after reload.
+            if ($scope.filter.kind && kinds_by_id[$scope.filter.kind.id] !== undefined) {
+                kinds = [kinds_by_id[$scope.filter.kind.id]];
+            } else {
+                kinds = $scope.kinds;
+            }
 
-        //     // We track kinds by their ids to ensure that the selected kind is
-        //     // preserved on language switch. Also note that in rare cases
-        //     // a kind might be gone after reload.
-        //     if ($scope.filter.kind && kinds_by_id[$scope.filter.kind.id] !== undefined) {
-        //         kinds = [kinds_by_id[$scope.filter.kind.id]];
-        //     } else {
-        //         kinds = $scope.kinds;
-        //     }
+            $scope.ropeelements = [];
+            angular.forEach(kinds, function (kind) {
+                var elements = getElements(kind);
+                if (elements.length > 0) {
+                    $scope.ropeelements.push({
+                        kind: kind,
+                        elements: elements
+                    });
+                }
+            });
+        }
 
-        //     $scope.ropeelements = [];
-        //     angular.forEach(kinds, function (kind) {
-        //         var elements = getElements(kind);
-        //         if (elements.length > 0) {
-        //             $scope.ropeelements.push({
-        //                 kind: kind,
-        //                 elements: elements
-        //             });
-        //         }
-        //     });
-        // }
-
-        // angular.forEach(filters, function (name) {
-        //     $scope.$watch('filter.' + name, function () {
-        //         // Prevent query on first watch
-        //         if ($scope.loaded) {
-        //             queryRopeElements();
-        //             storage.setItem('elementFilter', angular.toJson($scope.filter));
-        //         }
-        //     });
-        // });
+        angular.forEach(filters, function (name) {
+            $scope.$watch('filter.' + name, function () {
+                // Prevent query on first watch
+                if ($scope.loaded) {
+                    queryRopeElements();
+                    storage.setItem('elementFilter', angular.toJson($scope.filter));
+                }
+            });
+        });
     }
 ]);
 
