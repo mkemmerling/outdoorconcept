11e3856b0699d7af9cc6f4842e765301fb4fc464
Markus Kemmerling <markus.kemmerling@meduniwien.ac.at>
2015-01-29 07:30:08 +0100

    Simplified/cleaned upr ANgular routing.

--- main/static/main/js/routes.js
+++ main/static/main/js/routes.js
@@ -2,63 +2,38 @@
 
 angular.module('outdoorconcept.routes', ['ngRoute'])
     .config(['$routeProvider', 'languageProvider', function ($routeProvider, languageProvider) {
-        var en, de;
-        var default_url;
-
-        // TODO: Try templateUrl function instead?
-        // Or even with a :lang attribute (maybe use '/de/ng/seilelemente')
-        // We don't need to inject these values, but calling them on resolve
-        // allows us to set the language.
-        en = function (language) {
-            languageProvider.$get().setLanguage('en');
-            // language.setLanguage('en');
-            // console.warn("ROUTE", window.localStorage);
-            // return language;
-        };
-        // en.$inject = ['language'];
-
-        de = function (language) {
-            languageProvider.$get().setLanguage('de');
-            // console.warn("ROUTE", window.localStorage);
-            // language.setLanguage('de');
-            // return language;
-        };
-        // de.$inject = ['language'];
-
-
-        // TODO:
-        default_url = function () {
-            var lang = languageProvider.$get().getLanguage();
-            console.log("default_url", lang);
-            if (lang === 'de') {
-                return '/de/seilelemente';
-            } else {
-                return '/en/ropeelements';
-            }
-        };
-
+        var language = languageProvider.$get();
 
         $routeProvider
             .when('/en/ropeelements', {
-                templateUrl: '/en/ng/ropeelements',
-                controller: 'RopeElementListController',
-                resolve: {
-                    lang: en
-                }
+                templateUrl: function () {
+                    console.log("ROUTE en");
+                    language.setLanguage('en');
+                    return '/en/ng/ropeelements';
+                },
+                controller: 'RopeElementListController'
             })
             .when('/de/seilelemente', {
-                templateUrl: '/de/ng/ropeelements',
-                controller: 'RopeElementListController',
-                resolve: {
-                    lang: de
-                }
+                templateUrl: function () {
+                    console.log("ROUTE de");
+                    language.setLanguage('de');
+                    return '/de/ng/ropeelements';
+                },
+                controller: 'RopeElementListController'
             })
             .when('/de/siebert', {
                 templateUrl: '/ng/siebert',
                 controller: 'SiebertFormController'
             })
             .otherwise({
-                redirectTo: default_url
+                redirectTo: function () {
+                    console.log("ROUTE default_url", language.getLanguage());
+                    if (language.getLanguage() === 'de') {
+                        return '/de/seilelemente';
+                    } else {
+                        return '/en/ropeelements';
+                    }
+                }
             });
     }]);
 
