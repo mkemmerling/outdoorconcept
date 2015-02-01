40314da19187f1d0881c4bf34110c88ccff26d45
Markus Kemmerling <markus.kemmerling@meduniwien.ac.at>
2015-02-01 18:53:38 +0100

    Print oc logo and schema on pdf.

--- siebert/views.py
+++ siebert/views.py
@@ -1,11 +1,16 @@
 """Siebert form views."""
-from io import BytesIO
+
+import io
+import os
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from reportlab.pdfgen import canvas
+
+IMAGE_DIR = os.path.abspath(os.path.join(
+    __file__, os.pardir, 'static', 'siebert', 'images'))
 
 
 def siebert(request):
@@ -17,12 +22,18 @@ def siebert(request):
 
 def siebert_pdf(request):
     response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="oc_siebert.pdf"'
+    response['Content-Disposition'] = (
+        'attachment; filename="oc_siebert_form.pdf"')
 
-    buffer = BytesIO()
+    buffer = io.BytesIO()
     p = canvas.Canvas(buffer)
 
-    p.drawString(100, 100, "Hello world.")
+    # p.drawString(100, 100, "Hello world.")
+
+    logo = os.path.join(IMAGE_DIR, 'oc_logo.jpeg')
+    p.drawImage(logo, 400, 730, 151, 60)
+    schema = os.path.join(IMAGE_DIR, 'siebert_schema.jpeg')
+    p.drawImage(schema, 400, 580, 151, 143)
 
     p.showPage()
     p.save()
