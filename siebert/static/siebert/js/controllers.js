eeb5bbb532fc6717da69bd00748f7981eca1537e
Markus Kemmerling <markus.kemmerling@meduniwien.ac.at>
2015-02-02 08:18:44 +0100

    Siebert form WiP.

--- siebert/static/siebert/js/controllers.js
+++ siebert/static/siebert/js/controllers.js
@@ -2,10 +2,36 @@
 
 angular.module('outdoorconcept.siebert.controllers', [])
 .controller('SiebertFormController', ['$scope', '$window', function ($scope, $window) {
+    $scope.siebert = {};
 
     $scope.print = function () {
+        var date_options = {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric'
+            },
+            url = '/de/siebert/siebert.pdf',
+            params;
+
         // TODO: Validate required fields
-        $window.location.href = '/de/siebert/siebert.pdf';
+        // console.log('SiebertFormController', $scope.siebert);
+
+        params = angular.copy($scope.siebert);
+        if (params.flyingFox === '0') {
+            delete params.flyingFox;
+        }
+        if (angular.isObject(params.date)) {
+            params.date = params.date.toLocaleDateString('de-AT', date_options);
+        } else {
+            delete params.date;
+        }
+        params = $.param(params);
+        if (params) {
+            url += '?' + params;
+        }
+
+        // console.log('SiebertFormController', url);
+        $window.location.href = url;
     };
 
 }]);
--- siebert/views.py
+++ siebert/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
 
 IMAGE_DIR = os.path.abspath(os.path.join(
     __file__, os.pardir, 'static', 'siebert', 'images'))
@@ -20,31 +21,90 @@ def siebert(request):
         }, RequestContext(request))
 
 
+# 1 Centimeter = 28,3464567 Points
+# A4: 210 x 297 (595.27 x 841.89)
+
+cm1 = 28.35
+cm1_5 = 42.52
+cm2 = 56.68
+
+# Page borders
+bottom_left = (cm1_5, cm1_5)
+bottom_right = (A4[0] - cm1_5, cm1_5)
+top_left = (cm1_5, A4[1] - cm1_5)
+top_right = (A4[0] - cm1_5, A4[1] - cm1_5)
+
+
 def siebert_pdf(request):
+    # data = request.GET
+
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = (
         'attachment; filename="oc_siebert_formel.pdf"')
 
     buffer = io.BytesIO()
-    p = canvas.Canvas(buffer)
+    doc = canvas.Canvas(buffer, pagesize=A4, pageCompression=1)
 
-    p.drawString(20, 730, "Berechnung der Seilzugkraft bei horizontal "
-                 "gespannten Seiten")
-    p.drawString(20, 710, "Auf Basis der Siebert-Formel")
-    p.drawString(20, 695, "Überarbeitet unter Berücksichtigung der "
-                 "EN 15567:2013")
+    author = "www.outdoorconcept.at"
+    title = "Berechnung der Seilzugkraft bei horizontal gespannten Seiten"
+    subject = "Auf Basis der Siebert-Formel"
 
-    logo = os.path.join(IMAGE_DIR, 'oc_logo.jpeg')
-    p.drawImage(logo, 400, 730, 151, 60)
-    schema = os.path.join(IMAGE_DIR, 'siebert_schema.jpeg')
-    p.drawImage(schema, 400, 580, 151, 143)
-    formula = os.path.join(IMAGE_DIR, 'siebert_formula.jpeg')
-    p.drawImage(formula, 20, 580, 249, 65)
+    metadata(doc, author, title, subject)
+    helplines(doc)
+    header(doc, title, subject)
 
-    p.showPage()
-    p.save()
+    doc.showPage()
+    doc.save()
 
     pdf = buffer.getvalue()
     buffer.close()
     response.write(pdf)
     return response
+
+
+def metadata(doc, author, title, subject):
+    doc.setAuthor(author)
+    doc.setTitle(title)
+    doc.setSubject(subject)
+
+
+def helplines(doc):
+    doc.setLineWidth(.3)
+
+    pageborder = (
+        bottom_left + bottom_right,
+        bottom_right + top_right,
+        top_right + top_left,
+        top_left + bottom_left,
+    )
+    doc.lines(pageborder)
+
+
+def header(doc, title, subject):
+    doc.drawString(cm1_5, 790, title)
+    doc.drawString(cm1_5, 770, subject)
+    text = "Überarbeitet unter Berücksichtigung der EN 15567:2013"
+    doc.drawString(cm1_5, 755, text)
+
+    logo = os.path.join(IMAGE_DIR, 'oc_logo.jpeg')
+    w_logo, h_logo = 151, 60
+    x, y = top_right[0] - w_logo, top_right[1] - h_logo
+    doc.drawImage(logo, x, y, w_logo, h_logo)
+
+    schema = os.path.join(IMAGE_DIR, 'siebert_schema.jpeg')
+    w, h = 151, 143
+    x, y = top_right[0] - w, top_right[1] - h_logo - h - 5
+    doc.drawImage(schema, x, y, w, h)
+
+    formula = os.path.join(IMAGE_DIR, 'siebert_formula.jpeg')
+    doc.drawImage(formula, cm1_5, 610, 249, 65)
+
+
+
+
+
+
+
+
+
+
