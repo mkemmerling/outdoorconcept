9a4b0daade900d2a430f19a464a8a6648c325c37
Markus Kemmerling <markus.kemmerling@meduniwien.ac.at>
2015-07-31 11:28:03 +0200

    Unified migrations.

--- ropeelements/migrations/0001_initial.py
+++ ropeelements/migrations/0001_initial.py
@@ -36,6 +36,35 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
+            name='Difficulty',
+            fields=[
+                ('id', models.AutoField(
+                    serialize=False, auto_created=True, primary_key=True,
+                    verbose_name='ID')),
+                ('order', models.PositiveIntegerField(
+                    db_index=True, editable=False)),
+                ('identifier', models.CharField(
+                    max_length=50, verbose_name='Identifier')),
+                ('identifier_en', models.CharField(
+                    null=True, max_length=50, verbose_name='Identifier')),
+                ('identifier_de', models.CharField(
+                    null=True, max_length=50, verbose_name='Identifier')),
+                ('lower_bound', models.SmallIntegerField(
+                    choices=[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5),
+                             (6, 6), (7, 7), (8, 8), (9, 9), (10, 10)],
+                    default=1, verbose_name='from')),
+                ('upper_bound', models.SmallIntegerField(
+                    choices=[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5),
+                             (6, 6), (7, 7), (8, 8), (9, 9), (10, 10)],
+                    default=10, verbose_name='to')),
+            ],
+            options={
+                'verbose_name': 'Difficulty',
+                'ordering': ('order',),
+                'verbose_name_plural': 'Difficulties',
+            },
+        ),
+        migrations.CreateModel(
             name='Element',
             fields=[
                 ('id', models.AutoField(
